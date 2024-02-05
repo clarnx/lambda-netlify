31728d820692e55a2bd7c4e96399749410ca7ee2
clarnx <clarenceonline@outlook.com>
2024-02-05 23:50:03 +0000

    update

--- netlify/functions/hello-world/src/main.rs
+++ netlify/functions/hello-world/src/main.rs
@@ -1,45 +1,35 @@
-use lambda_runtime::{service_fn, Error, LambdaEvent};
+use lambda_http::{
+    http::{Response, StatusCode},
+    run, service_fn, Error, IntoResponse, Request, RequestPayloadExt,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = service_fn(func);
-    lambda_runtime::run(func).await?;
-    Ok(())
+    run(service_fn(function_handler)).await
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct RequestEvent {
-    hello: String,
-}
+pub async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
+    let body = event.payload::<MyPayload>()?;
 
-async fn func(event: LambdaEvent<RequestEvent>) -> Result<Value, Error> {
-    let response = json!({
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": serde_json::to_string(&json!({"message": event.payload})).unwrap(),
-        "isBase64Encoded": false
-    });
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/json")
+        .body(
+            json!({
+              "message": "Hello World",
+              "payload": body,
+            })
+            .to_string(),
+        )
+        .map_err(Box::new)?;
 
     Ok(response)
 }
-// use lambda_runtime::{service_fn, LambdaEvent, Error};
-// use serde_json::{json, Value};
 
-// #[tokio::main]
-// async fn main() -> Result<(), Error> {
-//     let func = service_fn(func);
-//     lambda_runtime::run(func).await?;
-//     Ok(())
-// }
-
-// async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
-//     let (event, _context) = event.into_parts();
-//     let first_name = event["firstName"].as_str().unwrap_or("world");
-
-//     Ok(json!({ "message": format!("Hello, {}!", first_name) }))
-// }
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct MyPayload {
+    pub prop1: String,
+    pub prop2: String,
+}
