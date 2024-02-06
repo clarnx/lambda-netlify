cbd38e17cd997c273e0d4ea6a4a4d5f6c4b71f8c
clarnx <clarenceonline@outlook.com>
2024-02-06 18:00:10 +0000

    update

--- netlify/functions/hello-world/src/main.rs
+++ netlify/functions/hello-world/src/main.rs
@@ -1,38 +1,33 @@
+// use lambda_http::Response;
 // use lambda_runtime::{service_fn, Error, LambdaEvent};
 // use serde::{Deserialize, Serialize};
 // use serde_json::json;
 // use serde_json::Value;
 
 // #[tokio::main]
-// async fn main() -> Result<(), Error> {
+// async fn main() -> Result<Value, Error> {
 //     let func = service_fn(func);
 //     lambda_runtime::run(func).await?;
 //     Ok(())
 // }
 
-// struct Request {
-//     #[serde("ren")]
-// multiValueQueryStringParameters: Option<Value>,
-// "path": "/.netlify/functions/hello-world",
-// "queryStringParameters": {},
-// "rawQuery": "",
-// "rawUrl": "https://bespoke-sable-36b18d.netlify.app/.netlify/functions/hello-world"
-// }
+// // struct Request {
+// //     #[serde("ren")]
+// // multiValueQueryStringParameters: Option<Value>,
+// // "path": "/.netlify/functions/hello-world",
+// // "queryStringParameters": {},
+// // "rawQuery": "",
+// // "rawUrl": "https://bespoke-sable-36b18d.netlify.app/.netlify/functions/hello-world"
+// // }
 
 // async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
 //     let (parts, body) = event.into_parts();
 
-//     let response = json!({
-//         "statusCode": 200,
-//         "headers": {
-//             "Content-Type": "application/json"
-//         },
-//         "body": serde_json::to_string(&json!({"message": "success", "parts": parts, "body": body})).unwrap(),
-//         "isBase64Encoded": false
-//     });
+//     let response = Response::builder().status(200).body("body").unwrap();
 
-//     Ok(response)
+//     Ok(serde_json::to_string(response))
 // }
+
 // use lambda_runtime::{service_fn, LambdaEvent, Error};
 // use serde_json::{json, Value};
 
@@ -50,14 +45,13 @@
 //     Ok(json!({ "message": format!("Hello, {}!", first_name) }))
 // }
 
-
 // use lambda_http::{http::{StatusCode, Response}, run, service_fn, Error, IntoResponse, Request, RequestExt, RequestPayloadExt};
 // use serde::{Deserialize, Serialize};
 // use serde_json::json;
 
 // #[tokio::main]
 // async fn main() -> Result<(), Error> {
-    
+
 //     run(service_fn(function_handler)).await
 // }
 
@@ -84,79 +78,70 @@
 //     pub prop2: String,
 // }
 
-//! This is an example function that leverages the Lambda Rust runtime HTTP support
-//! and the [axum](https://docs.rs/axum/latest/axum/index.html) web framework.  The
-//! runtime HTTP support is backed by the [tower::Service](https://docs.rs/tower-service/0.3.2/tower_service/trait.Service.html)
-//! trait.  Axum's applications are also backed by the `tower::Service` trait.  That means
-//! that it is fairly easy to build an Axum application and pass the resulting `Service`
-//! implementation to the Lambda runtime to run as a Lambda function.  By using Axum instead
-//! of a basic `tower::Service` you get web framework niceties like routing, request component
-//! extraction, validation, etc.
-use axum::extract::Query;
-use axum::http::StatusCode;
-use axum::{
-    extract::Path,
-    response::Json,
-    routing::{get, post},
-    Router,
-};
-use lambda_http::{run, Error};
-use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
-use std::env::set_var;
+// #[tokio::main]
+// async fn main() -> Result<(), Error> {
+//     // If you use API Gateway stages, the Rust Runtime will include the stage name
+//     // as part of the path that your application receives.
+//     // Setting the following environment variable, you can remove the stage from the path.
+//     // This variable only applies to API Gateway stages,
+//     // you can remove it if you don't use them.
+//     // i.e with: `GET /test-stage/todo/id/123` without: `GET /todo/id/123`
+//     set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
 
-#[derive(Deserialize, Serialize)]
-struct Params {
-    first: Option<String>,
-    second: Option<String>,
-}
+//     let app = Router::new()
+//         .route("/", get(root))
+//         .route("/foo", get(get_foo).post(post_foo))
+//         .route("/foo/:name", post(post_foo_name))
+//         .route("/parameters", get(get_parameters))
+//         .route("/health/", get(health_check));
 
-async fn root() -> Json<Value> {
-    Json(json!({ "msg": "I am GET /" }))
-}
+//     run(app).await
+// }
 
-async fn get_foo() -> Json<Value> {
-    Json(json!({ "msg": "I am GET /foo" }))
-}
+// use lambda_runtime::{service_fn, Error, LambdaEvent};
+// use serde_json::{json, Value};
 
-async fn post_foo() -> Json<Value> {
-    Json(json!({ "msg": "I am POST /foo" }))
-}
+// async fn handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
+//     let payload = event.payload;
+//     let first_name = payload["firstName"].as_str().unwrap_or("world");
+//     Ok(
+//         json!({ "message": format!("Hello, {first_name}!"), "statusCode": 200,  "headers": {
+//             "Content-Type": "application/json"
+//         }, }),
+//     )
+// }
 
-async fn post_foo_name(Path(name): Path<String>) -> Json<Value> {
-    Json(json!({ "msg": format!("I am POST /foo/:name, name={name}") }))
-}
+// #[tokio::main]
+// async fn main() -> Result<(), Error> {
+//     lambda_runtime::run(service_fn(handler)).await
+// }
 
-async fn get_parameters(Query(params): Query<Params>) -> Json<Value> {
-    Json(json!({ "request parameters": params }))
-}
-
-/// Example on how to return status codes and data from an Axum function
-async fn health_check() -> (StatusCode, String) {
-    let health = true;
-    match health {
-        true => (StatusCode::OK, "Healthy!".to_string()),
-        false => (StatusCode::INTERNAL_SERVER_ERROR, "Not healthy!".to_string()),
-    }
-}
+use aws_lambda_events::encodings::Body;
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use aws_lambda_events::http;
+use http::header::HeaderMap;
+use lambda_runtime::{service_fn, Context, Error, LambdaEvent};
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // If you use API Gateway stages, the Rust Runtime will include the stage name
-    // as part of the path that your application receives.
-    // Setting the following environment variable, you can remove the stage from the path.
-    // This variable only applies to API Gateway stages,
-    // you can remove it if you don't use them.
-    // i.e with: `GET /test-stage/todo/id/123` without: `GET /todo/id/123`
-    set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
+    let func = service_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
 
+pub(crate) async fn my_handler(
+    event: LambdaEvent<Value>,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    let request = serde_json::to_string(&event.payload).unwrap();
 
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/foo", get(get_foo).post(post_foo))
-        .route("/foo/:name", post(post_foo_name))
-        .route("/parameters", get(get_parameters))
-        .route("/health/", get(health_check));
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: HeaderMap::new(),
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Text(request)),
+        is_base64_encoded: false,
+    };
 
-    run(app).await
+    Ok(resp)
 }
